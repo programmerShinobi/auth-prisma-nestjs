b7f23ee3f1033822d1cc689348ecabaee3a21708
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-07-17 20:47:25 +0700

    add users

---
+++ src/users/users.controller.ts
@@ -0,0 +1,18 @@
+import {
+  Controller,
+  Post,
+  Body
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User as UserModel } from '@prisma/client';
+
+@Controller()
+export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
+  @Post('user')
+  async signupUser(
+    @Body() userData: { name?: string; email: string },
+  ): Promise<UserModel> {
+    return this.usersService.createUser(userData);
+  }
+}
