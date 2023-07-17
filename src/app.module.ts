b47a324ca3d5120088470ffd7abf6e332cc2bce2
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-07-17 20:47:45 +0700

    update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule, PostsModule]
 })
 export class AppModule {}
