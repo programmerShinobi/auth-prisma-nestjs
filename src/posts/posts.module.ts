1df7683a394483e5455c1fbf32bf3c225433bc5d
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-07-18 14:21:44 +0700

    add JwtStrategy in providers

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { PrismaService } from 'prisma/prisma.service';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
 @Module({
   controllers: [PostsController],
-  providers: [PostsService, PrismaService]
+  providers: [PostsService, PrismaService, JwtStrategy]
 })
 export class PostsModule {}
