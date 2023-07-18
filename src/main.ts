3b6a2d635188342a3d3aa182c55d3f4aa6d28bb7
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-07-18 12:07:16 +0700

    add cookieParser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from 'src/app.module';
+import { ValidationPipe } from '@nestjs/common';
+// import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
+import { clientUrl } from './utils/constants';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: clientUrl, credentials: true },
+  });
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  // app.use(helmet());
+
+  app.use(cookieParser());
+
   await app.listen(3000);
 }
 bootstrap();
