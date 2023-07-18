f83ac36caa989eff03e8ce7dfe9ace8375c02867
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-07-18 10:48:35 +0700

    add auth.dto

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, IsString, IsEmail, Length } from 'class-validator';
+
+export class AuthDto {
+  @IsNotEmpty()
+  @IsString()
+  public name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  public email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(3, 20, { message: 'Passowrd has to be at between 3 and 20 chars' })
+  public password: string;
+  
+}
