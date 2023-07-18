0cd360c4751595821034155aff588892407c5215
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-07-18 13:46:59 +0700

    update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { PrismaModule } from 'prisma/prisma.module';
 @Module({
-  imports: [AuthModule, UsersModule, PostsModule, PrismaModule]
+  imports: [AuthModule, PostsModule, PrismaModule]
 })
 export class AppModule {}
