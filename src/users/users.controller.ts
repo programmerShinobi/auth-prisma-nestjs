88da84e941cceb23e4b6f6c7693c7cdc856e35da
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-07-18 10:47:48 +0700

    update user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,7 +11,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) { }
   @Post('user')
   async signupUser(
-    @Body() userData: { name?: string; email: string },
+    @Body() userData: { name?: string; email: string; hashedPassword: string },
   ): Promise<UserModel> {
     return this.usersService.createUser(userData);
   }
