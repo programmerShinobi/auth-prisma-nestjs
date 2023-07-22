e35cce0e577459af523c8ec16cea950dfc01f3a5
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-07-22 15:13:22 +0700

    adding prisma seed

---
+++ prisma/seeding/seeder.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+import { users } from "./users";
+import * as bcrypt from 'bcryptjs';
+
+const prisma = new PrismaClient();
+
+  async function hashPassword(password: string): Promise<string> {
+    const saltOrRounds = 10;
+
+    return await bcrypt.hash(password, saltOrRounds);
+  }
+
+async function main(): Promise<void> {
+  for (let user of users) {
+    const hashedPassword = await hashPassword(user.hashedPassword);
+    user.hashedPassword = hashedPassword;
+    await prisma.user.create({
+      data: user
+    });
+    await prisma.post.create({
+      data: {
+        title: `Shinobi Series : ${Math.floor(Math.random() * 100)}`,
+        content: `Shinobi is a term commonly associated with the Naruto series, which is a popular anime and manga franchise. In the series, shinobi are highly skilled warriors who possess a wide range of abilities, including ninjutsu, taijutsu, and genjutsu. They are trained in various forms of combat and espionage, and are often tasked with completing dangerous missions on behalf of their village. The concept of shinobi has become a popular cultural icon, and has been featured in various forms of media outside of the Naruto series.`,
+        published: Math.random() < 0.5,
+        author: {
+          connect: { email: user.email }
+        }
+      },
+    });
+  }
+}
+
+main().catch((err) => {
+  console.log(err);
+  process.exit(1);
+}).finally(() => {
+  prisma.$disconnect();
+})
---
+++ prisma/seeding/users.ts
@@ -0,0 +1,104 @@
+
+export const users = [
+  {
+    name: "Naruto Uzumaki",
+    email: "naruto.uzumaki@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Sasuke Uchiha",
+    email: "sasuke.uchiha@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Sakura Haruno",
+    email: "sakura.haruno@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Kakashi Hatake",
+    email: "kakashi.hatake@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Hinata Hyuga",
+    email: "hinata.hyuga@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Shikamaru Nara",
+    email: "shikamaru.nara@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Neji Hyuga",
+    email: "neji.hyuga@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Rock Lee",
+    email: "rock.lee@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Tenten",
+    email: "tenten@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Gaara",
+    email: "gaara@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Kiba Inuzuka",
+    email: "kiba.inuzuka@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Hashirama Senju",
+    email: "hashirama.senju@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Tsunade",
+    email: "tsunade@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    
+    name: "Jiraiya",
+    email: "jiraiya@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Orochimaru",
+    email: "orochimaru@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Itachi Uchiha",
+    email: "itachi.uchiha@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Nagato Uzumaki",
+    email: "nagato.uzumaki@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Konan",
+    email: "konan@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Madara Uchiha",
+    email: "madara.uchiha@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+  {
+    name: "Obito Uchiha",
+    email: "obito.uchiha@shinobi.com",
+    hashedPassword: "isPassw0rd!",
+  },
+];
